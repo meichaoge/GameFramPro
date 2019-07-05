98d181030796a3b30544b2bee3302c8b32c55bb6
chaoge <386586291@qq.com>
2019-07-05 20:33:56 +0800

    更新了单例模板+新增资源加载的部分实现+补充内置类型的扩展方法
    Signed-off-by: chaoge <386586291@qq.com>

---
+++ GameFramePro/Assets/Scripts/Core/Base/Manager/ResourcesManager/AssetBundleManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace GameFramePro
+{
+    //
+
+    /// <summary>
+    /// AssetBundle 资源管理器
+    /// </summary>
+    public class AssetBundleManager : Single<AssetBundleManager>
+    {
+
+        #region Date
+        private Dictionary<string, AssetBundle> mAllLoadedAssetBundleAssets = new Dictionary<string, AssetBundle>(); //所有加载的AssetBundle 资源
+        #endregion
+
+        //同步加载AssetBundle 方法
+        public void LoadAsserBundleAssetSync(string assetBundlePath, string assetName, System.Action<UnityEngine.Object> loadCallback)
+        {
+
+        }
+
+
+    }
+}
