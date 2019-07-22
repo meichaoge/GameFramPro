5b650df04cde9c22a2c6c13dafb5d51619f13cb6
chaoge <386586291@qq.com>
2019-07-22 19:49:09 +0800

    封装了PlayerPrefs ,管理所有新增和删除key

---
+++ GameFramePro/Assets/Scripts/Core/Base/Module/PlayerPrefs/PlayerPrefsManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace GameFramePro
+{
+    /// <summary>
+    /// 封装Unity  PlayerPrefs 。方便统计所有本地化的的数据
+    /// </summary>
+    public class PlayerPrefsManager : Single<PlayerPrefsManager>
+    {
+        private const string TotalPlayerPrefsKeyRecord = "totalPlayerPrefsKeyRecordKeys";
+
+        private HashSet<string> mAllPlayerPrefsKeys = new HashSet<string>();
+
+        protected override void InitialSingleton()
+        {
+            base.InitialSingleton();
+        }
+
+
+
+        #region 封装接口
+        /// <summary>
+        /// TODO 需要封装对 TotalPlayerPrefsKeyRecord 的过滤
+        /// </summary>
+        public static void DeleteAll() { PlayerPrefs.DeleteAll(); }
+        public static void DeleteKey(string key) { PlayerPrefs.DeleteKey(key); }
+        public static void Save() { PlayerPrefs.Save(); }
+        public static bool HasKey(string key) { return PlayerPrefs.HasKey(key); }
+
+
+
+
+
+
+
+        public static float GetFloat(string key, float defaultValue=0f)
+        {
+            return PlayerPrefs.GetFloat(key, defaultValue);
+        }
+        public static int GetInt(string key, int defaultValue=0)
+        {
+            return PlayerPrefs.GetInt(key, defaultValue);
+        }
+        public static string GetString(string key, string defaultValue="")
+        {
+            return PlayerPrefs.GetString(key, defaultValue);
+        }
+
+
+
+
+        public static void SetFloat(string key, float value )
+        {
+            PlayerPrefs.SetFloat(key, value);
+        }
+        public static void SetInt(string key, int value)
+        {
+            PlayerPrefs.SetInt(key, value);
+        }
+        public static void SetString(string key, string value)
+        {
+            PlayerPrefs.SetString(key, value);
+        }
+
+        #endregion
+
+
+
+    }
+}
---
+++ GameFramePro/Assets/Scripts/Expand/FlagEnumAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/// <summary>
+/// 枚举属性显示成LayerMask 类似样式
+/// </summary>
+public class FlagEnumAttribute 
+{
+    private string headTitle;
+
+    /// <summary>
+    ///使用[EnumFlagsAttribute("......")] 情况
+    /// </summary>
+    /// <param name="title"></param>
+    public FlagEnumAttribute(string title)
+    {
+        headTitle = title;
+    }
+
+
+    public FlagEnumAttribute()
+    {
+
+    }
+}
