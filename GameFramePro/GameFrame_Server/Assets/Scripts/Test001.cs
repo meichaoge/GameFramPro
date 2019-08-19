d357b9e3acafb54cbd309c314a50a8cacca043e5
chaoge <386586291@qq.com>
2019-08-19 19:52:02 +0800

    Server端

--- GameFramePro/GameFrame_Server/Assets/Scripts/Test001.cs
+++ GameFramePro/GameFrame_Server/Assets/Scripts/Test001.cs
@@ -7,7 +7,7 @@ public class Test001 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Server");
     }
 
     // Update is called once per frame
