6e99cead6444a571e99a0f740b3448cb40026770
chaoge <386586291@qq.com>
2019-08-19 20:35:29 +0800

    Server端 简单的UDP 实现，不完善

---
+++ GameFramePro/GameFrame_Server/Assets/Scripts/UdpServer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+public class UdpServer : IDisposable
+{
+    private Socket mServerSocket;
+    private int mBindPort;
+    private const int BufferLength = 65536;
+    private byte[] mBuffer = new byte[BufferLength];
+
+    private Thread receiveThread;
+
+    #region 构造函数
+
+    public UdpServer()
+    {
+    }
+
+    public UdpServer(int endport)
+    {
+        mBindPort = endport;
+    }
+
+    #endregion
+
+    #region 接口
+
+    public void StartServer()
+    {
+        mServerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        mServerSocket.Bind(new IPEndPoint(IPAddress.Any, mBindPort));
+        receiveThread = new Thread(ReceiveData);
+        receiveThread.Start();
+    }
+
+    #endregion
+
+
+    #region 内部实现
+
+    private void ReceiveData(object obj)
+    {
+        while (true)
+        {
+            System.Array.Clear(mBuffer, 0, BufferLength);
+
+            int dataLength = mServerSocket.Receive(mBuffer, SocketFlags.None);
+            if (dataLength > 0)
+            {
+                string message = Encoding.UTF8.GetString(mBuffer, 0, dataLength);
+                Debug.Log($"接受到消息 {message}");
+            }
+
+            Thread.Sleep(100);
+        }
+    }
+
+    #endregion
+
+
+    public void Dispose()
+    {
+    }
+}
---
+++ GameFramePro/GameFrame_Server/Assets/Scripts/UdpServerUIComponent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UdpServerUIComponent : MonoBehaviour
+{
+    public InputField mListennerPort;
+    public Button mListennerButton;
+    public Text mReceiveMessageText;
+
+    public Text mSendMessageText;
+    public InputField mSendIpAddress;
+    public InputField mSendEndPort;
+    public InputField mSendMessage;
+    public Button mSendMessageButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mListennerButton.onClick.AddListener(OnListennnerButtonClick);
+        mSendMessageButton.onClick.AddListener(OnSendMessageButtonClick);
+    }
+
+
+    private void OnListennnerButtonClick()
+    {
+    }
+
+    private void OnSendMessageButtonClick()
+    {
+    }
+
+
+    public void ReceiveMessage(string message)
+    {
+    }
+
+    public void SendMessage(string message)
+    {
+    }
+}
