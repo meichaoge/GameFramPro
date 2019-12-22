da01a40bf75355417a95ba4c0c006f666aa2cbb7
meichao <386586291@qq.com>
2019-12-22 21:10:01 +0800

    基于深度法线纹理的边缘检测

---
+++ GameFramePro/Learn_UnityShader/LearnUnityShader/Assets/Scripts/Chapter 13/EdgeDetectNormalsAndDepth.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EdgeDetectNormalsAndDepth : BasePostProcessEffect
+{
+
+    [Range(0.0f, 1.0f)]
+    public float edgesOnly = 0.0f;
+
+    public Color edgeColor = Color.black;
+
+    public Color backgroundColor = Color.white;
+
+    public float sampleDistance = 1.0f;
+
+    public float sensitivityDepth = 1.0f;
+
+    public float sensitivityNormals = 1.0f;
+
+    void OnEnable()
+    {
+        GetComponent<Camera>().depthTextureMode |= DepthTextureMode.DepthNormals;
+    }
+
+    [ImageEffectOpaque]
+    protected override void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (mTargetMaterial != null)
+        {
+            mTargetMaterial.SetFloat("_EdgeOnly", edgesOnly);
+            mTargetMaterial.SetColor("_EdgeColor", edgeColor);
+            mTargetMaterial.SetColor("_BackgroundColor", backgroundColor);
+            mTargetMaterial.SetFloat("_SampleDistance", sampleDistance);
+            mTargetMaterial.SetVector("_Sensitivity", new Vector4(sensitivityNormals, sensitivityDepth, 0.0f, 0.0f));
+
+            Graphics.Blit(src, dest, mTargetMaterial);
+        }
+        else
+        {
+            Graphics.Blit(src, dest);
+        }
+    }
+}
