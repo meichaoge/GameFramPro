818925cd8d9b10a717c31ac902f7bd4c2e6a83a8
meichao <386586291@qq.com>
2019-12-27 16:52:53 +0800

    > 增加螺旋线接口 <2> 增加描边效果插件

--- GameFramePro/GameFrame_Client/Assets/Scripts/Core/Expand&Utility/MathfUtility.cs
+++ GameFramePro/GameFrame_Client/Assets/Scripts/Core/Expand&Utility/MathfUtility.cs
@@ -108,5 +108,143 @@ namespace GameFramePro
 
         #endregion
 
+
+        #region 贝塞尔曲线
+        public static Vector3 Bezier(float t, List<Vector3> points)
+        {
+            if (points == null || points.Count == 0)
+                return Vector3.zero;
+            if (points.Count == 1)
+                return points[0];
+
+            List<Vector3> temp = new List<Vector3>(points.Count - 1);
+            for (int dex = 0; dex < points.Count - 1; dex++)
+            {
+                Vector3 lerpPoint = (1 - t) * points[dex] + t * points[dex + 1];
+                temp.Add(lerpPoint);
+            }
+            return Bezier(t, temp);
+        }
+
+        public static Vector3 Bezier(float t, List<Transform> transPoints)
+        {
+            if (transPoints == null || transPoints.Count == 0)
+                return Vector3.zero;
+            if (transPoints.Count == 1)
+                return transPoints[0].position;
+
+            List<Vector3> temp = new List<Vector3>(transPoints.Count - 1);
+            for (int dex = 0; dex < transPoints.Count - 1; dex++)
+            {
+                Vector3 lerpPoint = (1 - t) * transPoints[dex].position + t * transPoints[dex + 1].position;
+                temp.Add(lerpPoint);
+            }
+            return Bezier(t, temp);
+        }
+        /// <summary>
+        /// 三个点之间的线性插值
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="po"></param>
+        /// <param name="p1"></param>
+        /// <param name="p2"></param>
+        /// <returns></returns>
+        public static Vector3 Bezier(float t, Vector3 po, Vector3 p1, Vector3 p2)
+        {
+            Vector3 temp1 = (1 - t) * po + t * p1;
+            Vector3 temp2 = (1 - t) * p1 + t * p2;
+            return (1 - t) * temp1 + t * temp2;
+        }
+
+        /// <summary>
+        /// 取两个向量 start,end 的中间向量的一半，然后缩放halfDistanceScale 作为中间点向量，然后进行二阶插值
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <param name="halfDistanceScale">中间向量缩放背书</param>
+        /// <returns></returns>
+        public static Vector3 Bezier(float t, Vector3 start, Vector3 end, float halfDistanceScale)
+        {
+            Vector3 middlePoint = 0.5f * halfDistanceScale * (start + end);
+
+            Vector3 temp1 = (1 - t) * start + t * middlePoint;
+            Vector3 temp2 = (1 - t) * middlePoint + t * end;
+            return (1 - t) * temp1 + t * temp2;
+        }
+        #endregion
+
+
+        #region 螺旋线
+        /// <summary>
+        /// 对数螺旋线 (  r = a * e.power(b * θ))
+        /// </summary>
+        /// <param name="t">插值数据[0,1]</param>
+        /// <param name="center">中心点坐标</param>
+        /// <param name="a">参数用来修改影响 范围</param>
+        /// <param name="b">参数用来控制角度变化快慢</param>
+        /// <param name="turns">转过的圈数</param>
+        /// <returns></returns>
+        public static Vector3 LogarithmicSpiral(float t, Vector3 center, float a = 0.3f, float b = 0.25f, float turns = 2.5f, bool isClockWise = false)
+        {
+            float angle = 2 * Mathf.PI * turns * t;  //旋转的角度
+            float radiu = a * Mathf.Exp(angle * b);
+
+            Vector3 offset;
+            if (isClockWise)
+                offset = new Vector3(radiu * Mathf.Cos(angle), 0, radiu * Mathf.Sin(angle));
+            else
+                offset = new Vector3(radiu * Mathf.Cos(angle), 0, -1 * radiu * Mathf.Sin(angle));
+
+            offset += center;
+            return offset;
+        }
+
+        /// <summary>
+        /// 阿基米德螺旋线 r=a+b* θ
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="center"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="turns"></param>
+        /// <returns></returns>
+        public static Vector3 ArchimedesSpiral(float t, Vector3 center, float a = 0.3f, float b = 0.25f, float turns = 2.5f, bool isClockWise = false)
+        {
+            float angle = 2 * Mathf.PI * turns * t;  //旋转的角度
+            float radiu = a + (angle * b);
+
+            Vector3 offset;
+            if (isClockWise)
+                offset = new Vector3(radiu * Mathf.Cos(angle), 0, -1 * radiu * Mathf.Sin(angle));
+            else
+                offset = new Vector3(radiu * Mathf.Cos(angle), 0, radiu * Mathf.Sin(angle));
+            offset += center;
+            return offset;
+        }
+
+        /// <summary>
+        /// 反向对数螺旋线 从半径distance 开始的泛型螺旋线
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="center"></param>
+        /// <param name="distance"></param>
+        /// <param name="turns"></param>
+        /// <returns></returns>
+        public static Vector2 OppositeLogarithmicSpiral(float t, Vector2 center, float distance = 10, float turns = 2.5f, bool isClockWise = false)
+        {
+            float angle = 2 * Mathf.PI * turns * t;  //旋转的角度
+            float radiu = distance * (Mathf.Exp(t) - 1) / (2.7182818f - 1);
+
+            Vector2 offset;
+            if (isClockWise)
+                offset = new Vector2(radiu * Mathf.Cos(angle), -1 * radiu * Mathf.Sin(angle));
+            else
+                offset = new Vector2(radiu * Mathf.Cos(angle), radiu * Mathf.Sin(angle));
+            offset += center;
+            return offset;
+        }
+        #endregion
+
     }
 }
